40f1ddbc4aadbe26bc20c983264edabbf608f9a0
harshitpolaris <harshit@polarisgrids.com>
2024-04-23 14:17:34 +0530

    title changed

--- lib/counter_mini_app.dart
+++ lib/counter_mini_app.dart
@@ -16,7 +16,7 @@ class CounterMiniApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Mini Counter App'),
     );
   }
 }
